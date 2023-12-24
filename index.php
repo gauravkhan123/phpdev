c6729db2593532719fb4eeea8a98fe42b694688e
gauravkhan123 <gauravkhan123@outlook.comgit config --global user.email gauravkhan123@outlook.comgit config --global user.email gauravkhan123@outlook.com>
2023-12-24 23:31:44 +0530

    add detail summary

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
     <h1>Welcome to PHP DEV</h1>
     <p>to learn PHP</p>
     <p>to write <code>CODE</code></p>
+    <details>
+  <summary>Epcot Center</summary>
+  <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international pavilions, award-winning fireworks and seasonal special events.</p>
+</details>
 </body>
 </html>
